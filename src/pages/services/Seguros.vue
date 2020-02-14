f33b611687b02014d75c6abebf3a2b3f12b98770
josemiguelq <jose.miguelq95@gmail.com>
2020-02-14 10:57:04 -0300

    seguro tawk to

--- src/pages/services/Seguros.vue
+++ src/pages/services/Seguros.vue
@@ -9,13 +9,13 @@
                         <option value="2">Mineração</option>
                     </select>
                 </div>
-                <input type="text" class="form-control" placeholder="Nome">
-                <input type="phone" class="form-control" placeholder="Contato">
-                <input type="email" class="form-control" placeholder="Email">
-                <input type="email" class="form-control" placeholder="CEP">
-                <input type="text" class="form-control" placeholder="Modelo">
-                <input type="text" class="form-control" placeholder="Série do equipamento">
-                <input type="text" class="form-control" placeholder="Horímetro">
+                <input type="text" class="form-control my-3" placeholder="Nome">
+                <input type="phone" class="form-control my-3" placeholder="Contato">
+                <input type="email" class="form-control my-3" placeholder="Email">
+                <input type="email" class="form-control my-3" placeholder="CEP">
+                <input type="text" class="form-control my-3" placeholder="Modelo">
+                <input type="text" class="form-control my-3" placeholder="Série do equipamento">
+                <input type="text" class="form-control my-3" placeholder="Horímetro">
 
                 <button class="primary-btn">Enviar</button>
             </form>
@@ -25,10 +25,19 @@
 
 <script>
     export default {
-        name: "Seguros"
+        name: "Seguros",
+        mounted() {
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            let a = Tawk_LoadStart;
+            a.getDate();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/5e3d5700a89cda5a1884b0ab/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
