30afe50522a5c634af37a7eed418265cb1830412
josemiguelq <jose.miguelq95@gmail.com>
2020-02-18 00:08:24 -0300

    posts

---
+++ src/components/blog/posts.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        id: "1",
+        title: "SEGURO: MAL NECESSÁRIO OU PROTEÇÃO INDISPENSÁVEL?",
+        text:
+            "Hoje apenas 30% da frota que está em atividade está segurada além disso muitos locadores não estão renovando as apólices...",
+        image: '/img/b2.jpg',
+        content: [
+            'Hoje apenas 30% da frota que está em atividade está segurada além disso muitos locadores não estão renovando as apólices.',
+            'Nesse momento difícil em que a quantidade de roubos sobe e o trabalho é escasso, resta a segurados e seguradoras terem uma negociação mais flexível para evitar prejuízos e manterem a integridade dos equipamentos.',
+            'Assim, fica a cargo do proprietário procurar proteção contra roubos de máquinas pesadas e agrícolas. Imagine se, ao ter o seu equipamento roubado e você ter perdido a oportunidade de estar com seu bem tão valioso protegido?',
+            '',
+        ]
+    },
+    {
+        id: '2',
+        title: "Operador não qualificado = Prejuízo para sua empresar",
+        text:
+            "Já pensou ter um equipamento danificado ou até mesmo a perda de uma carga ou estoque devido a um tombamento, atropelamento, batidas entre máquinas?...Casos como esses geram uma crise institucional e, consequentemente, perda nos lucros com a produção parada...",
+        image: '/img/blog/operador.png',
+        content: [
+            'Já pensou ter um equipamento danificado ou até mesmo a perda de uma carga ou estoque devido a um tombamento, atropelamento, batidas entre máquinas?',
+            'Casos como esses geram uma crise institucional e, consequentemente, perda nos lucros com a produção parada. Os maiores motivos relacionado a esses acidentes são operadores sem atenção e desconhecimento do equipamento. Mas tudo isso pode ser evitado simplesmente se  esses profissionais passarem por um treinamento específico.',
+            'A formação técnica de um operador não é suficiente vale ressaltar. Cabe a empresa também proporcionar ao profissional reciclagens periódicas e diálogos diários sobre a atividade a ser realizada.',
+            'Aqui você encontra operadores devidamente qualificados, contrate agora mesmo.',
+        ]
+    }
+]
